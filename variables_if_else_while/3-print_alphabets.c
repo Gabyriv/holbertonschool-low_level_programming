602e83564ec6cbd1c5f254606207257b0dab9c6f
Gabriel <gabyrivm06@gmail.com>
2024-06-18 13:04:49 -0400

    task 3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - Display alphabet in lowercase then in uppercase.
+ * main - Prints the alphabet.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		int i;
+	int i;
 
-		for (i = 0; i < 52; i++);
-		{
-			putchar(alp[i]);
-		}
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
 	putchar('\n');
 	return (0);
 }

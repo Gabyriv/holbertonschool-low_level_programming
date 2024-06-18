f97630ab44f84d01546d0669727c78bdb267f58e
Gabriel <gabyrivm06@gmail.com>
2024-06-18 13:21:37 -0400

    task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet except q and e.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char c;
+
+    for (c = 'a'; c <= 'z'; c++)
+    {
+        if (c != 'q' && c != 'e')
+        {
+            putchar(c);
+        }
+    }
+    putchar('\n');
+
+    return 0;
+}
+

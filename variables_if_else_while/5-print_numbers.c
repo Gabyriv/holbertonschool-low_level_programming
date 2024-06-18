15f643ae84b46b75b671f45e82f75ed86f14776c
Gabriel <gabyrivm06@gmail.com>
2024-06-18 13:40:39 -0400

    task 5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints all single digit numbers of base 10 starting from 0.
+ * main - Prints numbers from 0 to 9.
  *
  * Return: Always 0 (Success)
  */
@@ -14,5 +14,5 @@ int main(void)
 		printf("%d", i);
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }

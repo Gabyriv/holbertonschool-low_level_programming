e3c3f737f94f9ab1ec06a028bc2dda1ec3d0e62a
Gabriel <gabyrivm06@gmail.com>
2024-06-19 10:52:28 -0400

    task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Make the alphabet
+ *
+ * Return:void
+ */
+
+void print_alphabet(void)
+{
+	char al;
+	for (al = 'a'; al <= 'z'; al++)
+		_putchar(al);
+	_putchar('\n');
+}

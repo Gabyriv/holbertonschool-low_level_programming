6cc71701f9a464bf51a1f90a193eb836c0e977a2
Gabriel <gabyrivm06@gmail.com>
2024-06-19 11:30:20 -0400

    task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Make the aplhabet x10
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char alx10;
+	int i;
+	i = 0;
+	while (i < 10)
+	{
+	for (alx10 = 'a'; alx10 <= 'z'; alx10++);
+	}
+	{
+	_putchar(alx10);
+	}
+	{
+	_putchar('\n');
+	i++;
+	}
+}

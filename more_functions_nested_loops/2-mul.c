3782b53a2ab0678668486a486d83b0f50a2c40fe
Gabriel <gabyrivm06@gmail.com>
2024-06-20 10:14:44 -0400

    task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two intergers.
+ * @a: the first interger
+ * @b: the second interger
+ *
+ * Return: the result of the multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

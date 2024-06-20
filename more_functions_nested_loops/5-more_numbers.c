cb5c2538d276f595db2a3b11bba3621ffe7136cc
Gabriel <gabyrivm06@gmail.com>
2024-06-20 11:23:28 -0400

    task 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 /**
- * more_numbers - prints 10 times the numbers, from 0 to 14.
+ * more_numbers - prints 10 times the numbers,from 0 to 14.
  */
 
 void more_numbers(void)
 {
 	int i, j;
-	char buffer[150]; /* 15 numbers per line * 10 lines: 150 entries */
+	char num;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j > 9)
-				buffer[2 * j - 1] = (j / 10) + '0';
-			buffer[2 * j] = (j % 10) + '0';
+			if (j >= 10)
+			{
+				_putchar('1');
+			}
+			num = j % 10 + '0';
+			_putchar(num);
 		}
-		buffer[14 * (i + 1) + 1] = '\n';
+		_putchar('\n');
 	}
 
-	/* Using _putchar three times to print the entire buffer */
-	for (i = 0; i < 150; i++)
-		_putchar(buffer[i]);
 }

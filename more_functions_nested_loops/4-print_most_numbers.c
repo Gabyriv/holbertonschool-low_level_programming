172a3b7de70f1d4040ce0e0b945147d37b2a35ec
Gabriel <gabyrivm06@gmail.com>
2024-06-20 10:31:55 -0400

    task 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9 except 2 and 4, followed by a new line
+ */
+
+void print_most_numbers(void)
+{
+	char numbers[] = "01356789\n";
+	int i;
+
+	for (i = 0; numbers[i] != '\0'; i++)
+	{
+		_putchar(numbers[i]);
+	}
+}

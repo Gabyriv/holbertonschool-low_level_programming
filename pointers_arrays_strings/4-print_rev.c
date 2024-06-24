e18d1fbe108923c9042d781924752fb147a497b1
Gabriel <gabyrivm06@gmail.com>
2024-06-24 11:24:10 -0400

    task 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -7,13 +7,15 @@
 
 void print_rev(char *s)
 {
-	int len = 0, i;
+	int count = 0;
 
-	while (s[i++])
-		len++;
-
-	for (i = len - 1; i >= 0; i--)
-		_putchar(s[i]);
-
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		count++;
+	}
+	for (count--; count >= 0; count--)
+		_putchar(s[count]);
 	_putchar('\n');
 }

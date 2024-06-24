a347acb26267623aefd52537747989a61105b586
Gabriel <gabyrivm06@gmail.com>
2024-06-24 10:30:49 -0400

    task 2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen - Returns the length of a string
- * @s: the string to get the length of
+ * @s: the string  to get the length of
  *
  * Return: the length of @s
  */
@@ -10,7 +10,7 @@
 int _strlen(char *s)
 {
 	int len;
-	
+
 	len = 0;
 	while (*s != '\0')
 	{

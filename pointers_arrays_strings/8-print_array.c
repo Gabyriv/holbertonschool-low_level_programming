fb627d0053a0f82acecad7edb903495c905a51bf
Gabriel <gabyrivm06@gmail.com>
2024-06-24 12:48:51 -0400

    task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: The array integers
+ * @n: The numbers of integers
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i == n - 1)
+			continue;
+		printf(", ");
+	}
+	printf("\n");
+}

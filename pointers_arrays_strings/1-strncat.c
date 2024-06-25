4419231931856fbec6601df34f27091b0a6ea0fd
Gabriel <gabyrivm06@gmail.com>
2024-06-25 10:03:10 -0400

    task 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: The string to be concatenates upon
+ * @src: The string to be concatenates upon dest
+ * @n: The number of bytes.
+ *
+ * Return: A pointer to the resulting strings dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int = 0;
+	int dest_len = 0;
+
+	while (dest[i++])
+		dest_len++;
+
+	for (i = 0; src[i] && i < n; i++);
+	dest[dest_len++] = src[i];
+
+	return (dest);
+}

2914a845152aad84d83b58da572a3cb3501181e0
Gabriel <gabyrivm06@gmail.com>
2024-07-10 12:44:50 -0400

    task 3

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -7,65 +7,60 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 
 /**
- * op_add - Adds two variables.
- * @a: A variable.
- * @b: Another variable.
+ * op_add - Returns the sum of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: The sum of a + b.
+ * Return: The sum of a and b.
  */
-
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - Subtracts two variables.
- * @a: A variable.
- * @b: Another variable.
+ * op_sub - Returns the difference of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: The difference of a - b.
+ * Return: The difference of a and b.
  */
-
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
- * op_mul - Multiplies two variables.
- * @a: A variable.
- * @b: Another variable.
+ * op_mul - Returns the product of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: The product of a * b.
+ * Return: The product of a and b.
  */
-
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
- * op_div - Divides two numbers
- * @a: A number.
- * @b: Another number.
+ * op_div - Returns the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: The result of a / b.
+ * Return: The quotient of a and b.
  */
-
 int op_div(int a, int b)
 {
 	return (a / b);
 }
 
 /**
- * op_mod - Return the remainder of the division of two numbers.
- * @a: A number.
- * @b: Another number.
+ * op_mod - Returns the remainder of the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: The remainder of the division of a / b.
+ * Return: The remainder of the division of a by b.
  */
-
 int op_mod(int a, int b)
 {
 	return (a % b);

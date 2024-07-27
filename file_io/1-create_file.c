9f10bbc043eb8960b02ee7435e52753111abfe04
Gabriel <gabyrivm06@gmail.com>
2024-07-27 14:55:07 -0400

    task 1 code modified

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,34 +10,27 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+	int length = 0, file;
 
 	if (filename == NULL)
-	{
 		return (-1);
-	}
-
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (fd == -1)
-	{
-		return (-1);
-	}
 
 	if (text_content == NULL)
-	{
 		text_content = "";
+
+
+	while (text_content[length] != '\0')
+	{
+		length++;
 	}
 
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
-	rwr = write(fd, text_content, nletters);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (rwr == -1)
+	if (file == -1)
 		return (-1);
-	close(fd);
+
+	write(file, text_content, length);
 
 	return (1);
 }
+

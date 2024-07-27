7d33d6a7a3d3e38c4101d0db7d587c961c6c96da
Gabriel <gabyrivm06@gmail.com>
2024-07-27 10:40:50 -0400

    task 1 completed

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,26 +10,36 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fileOpen, fileWrite, length = 0;
+	int fileOpen, fileWrite, nletters;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
 
-	if (text_content != NULL)
+	fileOpen = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fileOpen == -1)
 	{
-		for (length = 0; text_content[length];)
-			length++;
+		return (-1);
 	}
 
-	fileOpen = open(filename, O_CREAT | O_RDWR | O_TRUNC | 0600);
-	fileWrite = write(fileOpen, text_content, length);
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
 
-	if (fileOpen == -1 || fileWrite == -1)
-		return (-1);
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
 
-	close(fileOpen);
+		fileWrite = write(fileOpen, text_content, nletters);
 
-	return (1);
+		if (fileWrite == -1)
+		{
+			return (-1);
+		}
+
+		close(fileOpen);
+
+		return (1);
 }

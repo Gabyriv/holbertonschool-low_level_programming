84dab0dcc7d9ac8cfbdfcb2bcbb2e142c0b11d75
Gabriel <gabyrivm06@gmail.com>
2024-07-27 15:04:26 -0400

    created the files for task 2

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: Is the name of the file.
+ * @text_content: The NULL terminated string to add at the end of the file.
+ *
+ * Return: 1 in success and -1 on failure.
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+

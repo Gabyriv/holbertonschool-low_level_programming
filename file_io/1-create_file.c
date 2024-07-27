ad0c0412c1fb800accec3dc7fbfeb148bc39a318
Gabriel <gabyrivm06@gmail.com>
2024-07-27 10:57:37 -0400

    task 1 completed

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -2,24 +2,26 @@
 
 /**
  * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A NULL terminated string to write to the file.
+ * @filename: The name of the file to create.
+ * @text_content: A NULL terminated string to write to the file
  *
- * Return: 1 on success, -1 on failure or if filename is NULL.
+ * Return: 1 on success, -1 otherwise.
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int fileOpen, fileWrite, nletters;
+	int fd;
+	int nletters;
+	int rwr;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
 
-	fileOpen = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (fileOpen == -1)
+	if (fd == -1)
 	{
 		return (-1);
 	}
@@ -31,15 +33,11 @@ int create_file(const char *filename, char *text_content)
 
 	for (nletters = 0; text_content[nletters]; nletters++)
 		;
+	rwr = write(fd, text_content, nletters);
 
-		fileWrite = write(fileOpen, text_content, nletters);
+	if (rwr == -1)
+		return (-1);
+	close(fd);
 
-		if (fileWrite == -1)
-		{
-			return (-1);
-		}
-
-		close(fileOpen);
-
-		return (1);
+	return (1);
 }

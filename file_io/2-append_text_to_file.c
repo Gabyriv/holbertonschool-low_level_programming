09a8b1126ca9c756a0b32a4f9efc34bf4ca0e8a1
Gabriel <gabyrivm06@gmail.com>
2024-07-28 08:28:46 -0400

    testing task #2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -10,4 +10,26 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int i = 0;
+	int file;
 
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+
+	file = open(filename, O_WRONLY | O_APPEND);
+
+	if (file == -1)
+		return (-1);
+
+	write(file, text_content, i);
+
+	return (1);
+}
